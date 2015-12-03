9990329c18eefca24b049fb379a99b0e1be18228
Taylor Bowland <taylor@taylorbowland.com>
2015-12-03 07:54:44 -0800

    Update unique checker to fix compiler warnings and accept a command line argument.

--- ctci/ch01_arrays_and_strings/01_all_unique.cpp
+++ ctci/ch01_arrays_and_strings/01_all_unique.cpp
@@ -2,13 +2,14 @@
 //
 // 1.1 - Implement an algorithm to determine if a string has all unique
 //       characters.
+//
+// Compile with:
+//   g++ -Wall -g -std=c++11 <file> -o <output>
 #include <algorithm>
 #include <iostream>
 #include <map>
+#include <stdio.h>
 #include <string>
-
-#include <assert.h>
-
 
 // Determine if a string has all unique characters.
 bool AllUnique(std::string word) {
@@ -24,7 +25,6 @@ bool AllUnique(std::string word) {
   return true;
 }
 
-
 // Determine if a string has all unique characters without using a supplemental
 // data structure. This sorts the string and then returns false as soon as it
 // identifies two identical consecutive characters.
@@ -33,7 +33,7 @@ bool AllUniqueNoDataStructure(std::string word) {
   std::sort(sorted_word.begin(), sorted_word.end());
 
   char& current = sorted_word.at(0);
-  for (int i = 1; i < sorted_word.length(); ++i) {
+  for (size_t i = 1; i < sorted_word.length(); ++i) {
     if (current == sorted_word.at(i)) {
       return false;
     }
@@ -42,10 +42,22 @@ bool AllUniqueNoDataStructure(std::string word) {
   return true;
 }
 
+// Determine if the provided string contains all unique characters.
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " <word>" << std::endl;
+    return 1;
+  }
 
-int main() {
-  std::string my_word = "thishasrepeats";
-  assert (AllUnique(my_word) == false);
-  assert (AllUniqueNoDataStructure(my_word) == false);
+  // The word itself.
+  std::string my_word = argv[1];
+
+  // Hacky way of setting the proper text.
+  std::string unique_text = "";
+  if (AllUniqueNoDataStructure(my_word) == false) {
+    unique_text = "not ";
+  }
+  std::cout << "Word '" << my_word << "' does "<< unique_text << "contain all "
+            << "unique characters." << std::endl;
   return 0;
 }
